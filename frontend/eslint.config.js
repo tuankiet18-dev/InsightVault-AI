291bb03990b5b719286942f5c900d021bbaf257f
Phạm Trần Đăng Phú <phamphu9102019@gmail.com>
2026-05-28 18:45:32 +0700

    initUI-MockUI

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -25,4 +25,10 @@ export default defineConfig([
       ],
     },
   },
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 ])
